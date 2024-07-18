d6f551544d1428cf56690d0ca1c6f3e78133cbab
Welington Augusto dos Santos <92633934+Welington-dos-Santos@users.noreply.github.com>
2024-07-18 17:23:05 -0300

    feat: Estilização do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,32 @@
-import  styles  from './Post.module.css';
+import styles from './Post.module.css';
 
 export function Post() {
     return (
         <article className={styles.post}>
-        <header>
-            <div className={styles.author}>
-                <img className={styles.avatar} src="https://github.com/Welington-dos-Santos.png"/>
-                <div className={styles.authorInfo}>
-                    <strong>Welington dos Santos</strong>
-                    <span>Web Developer</span>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/Welington-dos-Santos.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Welington dos Santos</strong>
+                        <span>Web Developer</span>
+                    </div>
                 </div>
+
+                <time title='18 de Julho as 11:06' datetime="18/07/2024 11:05:59">Publicado a 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galera</p>
+                <p>Acabei de subir mais um projeto no git</p>
+                <p>
+                    <a href='#'>welington.design/doctorcare</a>
+                </p>
+                <p>
+                    <a href='#'>#novoprojeto</a>{' '}
+                    <a href='#'>#nlw</a>{' '}
+                    <a href='#'>#rocketseat</a>
+                </p>
             </div>
-
-            <time title='18 de Julho as 11:06' datetime="18/07/2024 11:05:59"></time>
-        </header>
-
-        <div className={styles.content}>
-            <p>Fala galera</p>
-            <p>Acabei de subir mais um projeto no git</p>
-            <p><a href='#'>welington.design/doctorcare</a></p>
-            <p><a href='#'>#novoprojeto #nlw #rocketseat</a></p>
-        </div>
-    </article>
+        </article>
     )
 }
