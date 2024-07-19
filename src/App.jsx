7c3eb029afc14dd00c8c7475da79ae8669610c7e
Welington Augusto dos Santos <92633934+Welington-dos-Santos@users.noreply.github.com>
2024-07-19 16:44:59 -0300

    feat: Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 import { Post } from './components/Post'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Welington-dos-Santos.png',
+      name: 'Welington dos Santos',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera', },
+      { type: 'paragraph', content: 'Acabei de subir uma projeto', },
+      { type: 'link', content: 'welington.design/doctorcare', },
+    ],
+    publishedAt: new Date('2024-07-19 17:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://conteudo.imguol.com.br/c/bol/fotos/b9/2017/05/08/muralha-da-china-1494296179991_615x300.jpg',
+      name: 'Gabriel dos Santos',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera', },
+      { type: 'paragraph', content: 'Acabei de subir uma projeto', },
+      { type: 'link', content: 'welington.design/doctorcare', },
+    ],
+    publishedAt: new Date('2024-07-23 16:30:00')
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -13,8 +44,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
