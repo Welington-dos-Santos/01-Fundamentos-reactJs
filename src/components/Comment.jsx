60cc3d3aef0767c309c55ce06f1741977564742f
Welington Augusto dos Santos <welington.adossantos20@gmail.com>
2024-07-31 23:18:21 -0300

    feat: Comuninção entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/Welington-dos-Santos.png"/>
@@ -15,7 +20,7 @@ export function Comment({ content }) {
                             <time title='18 de Julho as 11:06' datetime="18/07/2024 11:05:59">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
                         </button>
                     </header>
